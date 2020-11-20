87c9fff1b4dae62f2fc6321ba3e612647dc406c6
maksimus <gorlovmaks23@gmail.com>
2020-11-21 01:47:53 +0600

    Добавила роуты для главных контейнеров приложения.

--- src/App.js
+++ src/App.js
@@ -6,6 +6,11 @@ import AppToolbar from "./components/AppToolbar/AppToolbar";
 import Register from "./containers/Register/Register";
 import Login from "./containers/Login/Login";
 import {useSelector} from "react-redux";
+import Posts from "./containers/Posts/Posts";
+import FullPost from "./containers/FullPost/FullPost";
+import NewPost from "./containers/NewPost/NewPost";
+import AlignItemsList from "./components/CommentCard/CommentCard";
+
 
 const App = () => {
     const user = useSelector(state => state.users.user);
@@ -16,8 +21,13 @@ const App = () => {
             <main>
                 <Container>
                     <Switch>
+                        <Route path="/" exact component={Posts} />
                         <Route path="/register" exact component={Register} />
                         <Route path="/login" exact component={Login} />
+                        <Route path="/posts/:id" exact component={FullPost}/>
+                        <Route path="/new_post" exact component={NewPost}/>
+                        <Route path="/comment" exact component={AlignItemsList}/>
+
                         <Route render={() => <h1>404 Not Found</h1>}/>
                     </Switch>
                 </Container>
